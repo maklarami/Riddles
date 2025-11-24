5f0cf234e2bcf7ef7ff93a7fb76b6d92520f6f0a
maklarami <klara.malinowska@gmail.com>
2025-11-24 10:52:22 +0100

    feature: Part 2, Day 5, 2024

--- src/aoc_2024/aoc_2024_05/aoc_2024_05.py
+++ src/aoc_2024/aoc_2024_05/aoc_2024_05.py
@@ -20,7 +20,7 @@ class Rules:
             for pos2 in range(pos1 + 1, len(update)):
                 second_page = update[pos2]
                 if second_page in self.is_before[first_page]:
-                    print(f"Violation: {second_page}|{update[pos1]} in {update}")
+                    print(f"Violation: {second_page}|{first_page} in {update}")
                     return False
                 if first_page in self.is_after[second_page]:
                     print(f"Violation: {first_page}|{second_page} in {update}")
@@ -28,10 +28,27 @@ class Rules:
 
         return is_conform
 
+    def reorder(self, update: list[int]) -> list[int]:
+        new_update = [update[0]]
 
-def parse_input(input: str) -> tuple[Rules, list[list[int]]]:
-    updates_list = []
+        for i in range(1,len(update)):
+            page_to_insert = update[i]
+            inserted = False
+            for j in range (len(new_update)):
+                page_to_compare = new_update[j]
+                if page_to_insert in self.is_before[page_to_compare]:
+                    new_update.insert(j, page_to_insert)
+                    inserted = True
+                    break
+
+            if not inserted:
+                new_update.append(page_to_insert)
+
+        return new_update
+
+def parse_input(input : str) -> tuple[Rules, list[list[int]]]:
     rules = Rules()
+    updates_list = []
     is_update = False
 
     for line in input.splitlines():
@@ -46,19 +63,34 @@ def parse_input(input: str) -> tuple[Rules, list[list[int]]]:
 
     return rules, updates_list
 
+def get_middle(lst: list[int]) -> int:
+    return lst[math.floor(len(lst)/2)]
+
 def solve_part_1(input : str) -> int:
     rules, updates_list = parse_input(input)
-    sum  = 0
 
+    sum  = 0
     for update in updates_list:
         if rules.is_conform(update):
             print(f"Hurray: {update}")
-            sum += update[math.floor(len(update)/2)]
+            sum += get_middle(update)
 
     return sum
 
+def solve_part_2(input : str) -> int:
+    rules, updates_list = parse_input(input)
+
+    sum  = 0
+    for update in updates_list:
+        if not rules.is_conform(update):
+            print(f"To reorder: {update}")
+            reorderd_update = rules.reorder(update)
+            print(f"Reordered: {reorderd_update}")
+            sum += get_middle(reorderd_update)
+    return sum
+
 if __name__ == "__main__":
-    print(solve_part_1("""47|53
+    print(solve_part_2("""47|53
 97|13
 97|61
 97|47
@@ -85,4 +117,4 @@ if __name__ == "__main__":
 75,29,13
 75,97,47,61,53
 61,13,29
-97,13,75,29,477"""))
+97,13,75,29,47"""))
--- src/aoc_2024/aoc_2024_05/test_aoc_2024_05.py
+++ src/aoc_2024/aoc_2024_05/test_aoc_2024_05.py
@@ -1,11 +1,10 @@
 from ...commons.commons import import_input
-from .aoc_2024_05 import solve_part_1
+from .aoc_2024_05 import solve_part_1, solve_part_2
 
 DAY = 5
 YEAR = 2024
 
-def test_part_1_example():
-    example = """47|53
+example = """47|53
 97|13
 97|61
 97|47
@@ -33,7 +32,12 @@ def test_part_1_example():
 75,97,47,61,53
 61,13,29
 97,13,75,29,47"""
+
+def test_part_1_example():
     assert solve_part_1(example) == 143
+
+def test_part_2_example():
+    assert solve_part_2(example) == 123
 
 def test_part_1_input():
     input = import_input(YEAR=YEAR, DAY=DAY)
@@ -41,3 +45,10 @@ def test_part_1_input():
     result = solve_part_1(input)
     print(f"Part 1 result: {result}")
     assert result == 3608
+
+def test_part_2_input():
+    input = import_input(YEAR=YEAR, DAY=DAY)
+
+    result = solve_part_2(input)
+    print(f"Part 2 result: {result}")
+    assert result == 4922
